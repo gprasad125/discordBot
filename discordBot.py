8d4626887d7c7cd4b88b7fd05722331325660ddf
gokul prasad <gokulprasad125@gmail.com>
2020-11-28 23:19:16 -0600

    added dotenv stuff

--- discordBot.py
+++ discordBot.py
@@ -1,6 +1,15 @@
-#Import statements
-import discord
-import os
+#Import statements:
+import discord #discord python library
+
+import os #helps with initialization + discord bot functionality
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#find discord token from env file in same area as discordBot.py
+token = os.getenv("DISCORD_TOKEN")
 
 #initialize client
 bot = discord.Client()
+
+#define bot events:
