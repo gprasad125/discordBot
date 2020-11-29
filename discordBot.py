33702fbaac98dcdfa775211f2813354499e1c4d5
gokul prasad <gokulprasad125@gmail.com>
2020-11-28 23:32:52 -0600

    figure out the env file stuff

--- discordBot.py
+++ discordBot.py
@@ -4,12 +4,20 @@ import discord #discord python library
 import os #helps with initialization + discord bot functionality
 from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 #find discord token from env file in same area as discordBot.py
-token = os.getenv("DISCORD_TOKEN")
+#token = os.getenv("DISCORD_TOKEN")
 
 #initialize client
 bot = discord.Client()
 
 #define bot events:
+@bot.event
+async def sayHello(message):
+    if message.content == "Hello":
+        await message.channel.send("Hello! I hope you are well")
+
+
+#run the bot
+bot.run('NzgyNDE1NzM4MTQ0MDMwNzYw.X8L3cA.SW7mqqrJluV_GB_AAMNO8y0IUAM')
